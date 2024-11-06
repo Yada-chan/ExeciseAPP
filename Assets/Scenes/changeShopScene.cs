1df4f6e4e6d2f27480cd7421331751d37603d489
Yada-chan <chunaijucun@gmail.com>
2024-11-06 14:44:04 +0900

    Initial commit

---
+++ Assets/Scenes/changeShopScene.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeShopScene : MonoBehaviour
+{
+    public void change_button(){
+        SceneManager.LoadScene("shop");
+    }
+}
